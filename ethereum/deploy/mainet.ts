b67c3232548cba47ac130d454127eb9d0092a665
Lucas Sunsi <sunsi.lucas@gmail.com>
2020-11-19 14:46:51 -0300

    Typescript V2 (#68)

--- ethereum/deploy/mainet.ts
+++ ethereum/deploy/mainet.ts
@@ -1,11 +1,19 @@
-export default async ({ deployments: { deploy }, getNamedAccounts }) => {
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deploy: DeployFunction = async ({
+  deployments: { deploy },
+  getNamedAccounts,
+}: HardhatRuntimeEnvironment) => {
   const { deployer } = await getNamedAccounts();
 
   await deploy("Arbrito", {
     from: deployer,
     gasLimit: 600000,
-    gasPrice: 12000000000,
+    gasPrice: "12000000000",
     log: true,
     args: [],
   });
 };
+
+export default deploy;
