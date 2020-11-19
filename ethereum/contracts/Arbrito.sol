10a74db02f4982caeb33f8521347b96151e0bd08
Lucas Sunsi <sunsi.lucas@gmail.com>
2020-11-19 16:48:19 -0300

    Add reserve check for uniswap reserves on contract (#71)
    This should help lowering average gas spent, considering
    that we kind of miss a lot.

--- ethereum/contracts/Arbrito.sol
+++ ethereum/contracts/Arbrito.sol
@@ -11,12 +11,18 @@ contract Arbrito is IUniswapPairCallee {
     uint256 amount,
     address uniswapPair,
     address balancerPool,
-    uint256 blockNumber
+    uint256 blockNumber,
+    uint256 uniswapReserve0,
+    uint256 uniswapReserve1
   ) external {
     require(block.number == blockNumber, "Delayed execution");
     (uint256 amount0, uint256 amount1) = direction ? (amount, uint256(0)) : (uint256(0), amount);
 
-    bytes memory payload = abi.encode(balancerPool, msg.sender);
+    (uint256 reserve0, uint256 reserve1, ) = IUniswapPair(uniswapPair).getReserves();
+    require(reserve0 == uniswapReserve0, "Reserve0 mismatch");
+    require(reserve1 == uniswapReserve1, "Reserve1 mismatch");
+
+    bytes memory payload = abi.encode(balancerPool, msg.sender, reserve0, reserve1);
     IUniswapPair(uniswapPair).swap(amount0, amount1, address(this), payload);
   }
 
@@ -26,8 +32,9 @@ contract Arbrito is IUniswapPairCallee {
     uint256 amount1,
     bytes calldata data
   ) external override {
-    (address balancerPoolAddress, address ownerAddress) = abi.decode(data, (address, address));
-    IBalancerPool balancerPool = IBalancerPool(balancerPoolAddress);
+    (address balancerPoolAddress, address ownerAddress, uint256 reserve0, uint256 reserve1) =
+      abi.decode(data, (address, address, uint256, uint256));
+
     IUniswapPair uniswapPair = IUniswapPair(msg.sender);
 
     uint256 amountTrade;
@@ -40,12 +47,12 @@ contract Arbrito is IUniswapPairCallee {
     address tokenTrade;
 
     if (amount0 != 0) {
-      (reserveTrade, reservePayback, ) = uniswapPair.getReserves();
+      (reserveTrade, reservePayback) = (reserve0, reserve1);
       tokenPayback = uniswapPair.token1();
       tokenTrade = uniswapPair.token0();
       amountTrade = amount0;
     } else {
-      (reservePayback, reserveTrade, ) = uniswapPair.getReserves();
+      (reservePayback, reserveTrade) = (reserve0, reserve1);
       tokenPayback = uniswapPair.token0();
       tokenTrade = uniswapPair.token1();
       amountTrade = amount1;
@@ -56,7 +63,7 @@ contract Arbrito is IUniswapPairCallee {
     IERC20(tokenTrade).approve(balancerPoolAddress, amountTrade);
 
     (uint256 balancerAmountOut, ) =
-      balancerPool.swapExactAmountIn(
+      IBalancerPool(balancerPoolAddress).swapExactAmountIn(
         tokenTrade,
         amountTrade,
         tokenPayback,
