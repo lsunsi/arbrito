8554d6ba1ab50e7dc8e1e761c24bb190a7e11236
lsunsi <sunsi.lucas@gmail.com>
2020-12-04 18:31:09 -0300

    Update deployment configs

--- ethereum/deploy/mainet.ts
+++ ethereum/deploy/mainet.ts
@@ -9,10 +9,13 @@ const deploy: DeployFunction = async ({
 
   await deploy("Arbrito", {
     from: deployer,
-    gasLimit: 1_000_000,
+    gasLimit: 2_000_000,
     gasPrice: "0x4a817c800", // 20gwei
     log: true,
-    args: [],
+    args: [
+      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    ],
   });
 };
 
